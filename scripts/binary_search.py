3ec967aaa48ae9007d9c2676a30c2a307909e721
Delete Prod <martynjamesoconnor@hotmail.co.uk>
2021-12-13 21:24:36 +0000

    Initial commit of binary search example

---
+++ scripts/binary_search.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+
+# While loop to step through guesses chopping off half of all possibilities each time
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else: low = mid + 1
+    return None
+
+# Generate a list to search
+the_list = [1, 3, 5, 7, 9]
+
+# Example - return the index in the_list where the value 3 exists
+print(binary_search(the_list, 3))
+
+# Example - attempt to find the index where -1 is the value - returns None
+print(binary_search(the_list, -1))
